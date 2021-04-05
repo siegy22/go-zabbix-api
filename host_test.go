4d7a268b24195aec39ac1a2ba91df6fc85efe070
Hirotake Kobayashi <h.kobayashi@raksul.com>
2021-04-06 02:22:21 +0900

    Add test for selectInterfaces,selectMacros,selectParentTemplates

--- host_test.go
+++ host_test.go
@@ -51,6 +51,7 @@ func TestHosts(t *testing.T) {
 	if host.HostID == "" || host.Host == "" {
 		t.Errorf("Something is empty: %#v", host)
 	}
+	iface := host.Interfaces[0]
 	host.GroupIds = nil
 	host.Interfaces = nil
 
@@ -83,6 +84,83 @@ func TestHosts(t *testing.T) {
 	}
 	if len(hosts) != 1 {
 		t.Errorf("Bad hosts: %#v", hosts)
+	}
+
+	hosts, err = api.HostsGet(zapi.Params{
+		"hostids":               host.HostID,
+		"selectInterfaces":      "extend",
+		"selectMacros":          "extend",
+		"selectParentTemplates": "extend",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(hosts) != 1 {
+		t.Errorf("Bad hosts: %#v", hosts)
+	}
+	if len(hosts[0].Interfaces) != 1 {
+		t.Errorf("Bad interfaces: %#v", hosts[0].Interfaces)
+	}
+	iface2 := hosts[0].Interfaces[0]
+	iface.InterfaceID = iface2.InterfaceID
+	if !reflect.DeepEqual(iface, iface2) {
+		t.Errorf("Interfaces are not equal:\n%#v\n%#v", iface, iface2)
+	}
+	if len(hosts[0].Templates) != 0 {
+		t.Errorf("Templates is not empty: %#v", hosts[0].Templates)
+	}
+	if len(hosts[0].UserMacros) != 0 {
+		t.Errorf("UserMacros is not empty: %#v", hosts[0].UserMacros)
+	}
+
+	template := testCreateTemplate(group, t)
+	defer testDeleteTemplate(template, t)
+
+	iface = zapi.HostInterface{IP: "127.0.0.1", Port: "10050", Type: zapi.Agent, UseIP: 1, Main: 1}
+	host.Interfaces = zapi.HostInterfaces{iface}
+	host.TemplateIDs = zapi.TemplateIDs{{template.TemplateID}}
+	macro := zapi.Macro{MacroName: "{$NAME}", Value: "Value"}
+	host.UserMacros = zapi.Macros{macro}
+
+	err = api.HostsUpdate(zapi.Hosts{*host})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	hosts, err = api.HostsGet(zapi.Params{
+		"hostids":               host.HostID,
+		"selectInterfaces":      "extend",
+		"selectMacros":          "extend",
+		"selectParentTemplates": "extend",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(hosts) != 1 {
+		t.Errorf("Bad hosts: %#v", hosts)
+	}
+	if len(hosts[0].Interfaces) != 1 {
+		t.Errorf("Bad interfaces: %#v", hosts[0].Interfaces)
+	}
+	iface2 = hosts[0].Interfaces[0]
+	iface.InterfaceID = iface2.InterfaceID
+	if !reflect.DeepEqual(iface, iface2) {
+		t.Errorf("Interfaces are not equal:\n%#v\n%#v", iface, iface2)
+	}
+	if len(hosts[0].Templates) != 1 {
+		t.Errorf("Bad templates: %#v", hosts[0].Templates)
+	}
+	template2 := hosts[0].Templates[0]
+	if template.Host != template2.Host {
+		t.Errorf("Templates are not equal:\n%#v\n%#v", template.Host, template2.Host)
+	}
+	if len(hosts[0].UserMacros) != 1 {
+		t.Errorf("Bad userMacros: %#v", hosts[0].UserMacros)
+	}
+	macro2 := hosts[0].UserMacros[0]
+	macro.HostID = hosts[0].HostID
+	if !reflect.DeepEqual(macro, macro2) {
+		t.Errorf("UserMacros are not equal:\n%#v\n%#v", macro, macro2)
 	}
 
 	testDeleteHost(host, t)
