df614af1b5c8cad188ec08cf06235607db5ecfdf
Yves Siegrist <Elektron1c97@gmail.com>
2024-07-12 16:00:06 +1000

    Update host_interface.go

--- host_interface.go
+++ host_interface.go
@@ -27,13 +27,13 @@ type InterfaceDetails struct {
 // HostInterface represents zabbix host interface type
 // https://www.zabbix.com/documentation/3.2/manual/api/reference/hostinterface/object
 type HostInterface struct {
-	InterfaceID string `json:"interfaceid,omitempty"`
-	DNS         string           `json:"dns"`
-	IP          string           `json:"ip"`
-	Main        int              `json:"main"`
-	Port        string           `json:"port"`
-	Type        InterfaceType    `json:"type"`
-	UseIP       int              `json:"useip"`
+        InterfaceID string        `json:"interfaceid,omitempty"`
+	DNS         string        `json:"dns"`
+	IP          string        `json:"ip"`
+	Main        int           `json:"main,string"`
+	Port        string        `json:"port"`
+	Type        InterfaceType `json:"type,string"`
+	UseIP       int           `json:"useip,string"`
 	Details     InterfaceDetails `json:"details"`
 }
 
